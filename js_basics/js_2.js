3b986340a2e0a4e761a07760d9de6aad387fbf6d
Deepanshu Gupta <97518034+de3de3@users.noreply.github.com>
2023-12-24 07:36:07 +0000

    conversion of DT

---
+++ js_basics/js_2.js
@@ -0,0 +1,5 @@
+let apple = "old";
+const app = 2323;
+let cityName;
+
+console.table([apple,app,cityName])
---
+++ js_basics/js_3.js
@@ -0,0 +1,9 @@
+let name = "string"
+let app = 3
+
+
+let appname = String(app);
+console.log(typeof undefined)
+
+
+
